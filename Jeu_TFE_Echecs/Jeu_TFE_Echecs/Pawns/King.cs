cd21a363006bf19f05f41c0de4b074579fd2c566
Leemans Nathan <200253@CCM-Asty-Moulin.local>
2023-02-07 14:25:02 +0100

    first update

---
+++ Jeu_TFE_Echecs/Jeu_TFE_Echecs/Pawns/King.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jeu_TFE_Echecs
+{
+    class King : Piece
+    {
+        private bool _check;
+        private bool _checkMate;
+
+        public King(int[] position, string color) : base(position, color)
+        {
+            _check = false;
+            _checkMate = false;
+        }
+    }
+}
---
+++ Jeu_TFE_Echecs/Jeu_TFE_Echecs/Pawns/Tower.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jeu_TFE_Echecs
+{
+    class Tower : Piece
+    {
+        public Tower(int[] position, string color) : base(position, color)
+        {
+
+        }
+    }
+}
