60cde55dc0ca19668391c04797269fa91fe586c1
Leemans Nathan <200253@CCM-Asty-Moulin.local>
2023-02-14 16:03:24 +0100

    page html

---
+++ Regle_jeu/Regle_jeu.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/base.css">
+        <title>Règle du jeu</title>
+    </head>
+    <body>
+        <div class="navbar">
+            <ul>
+                <li><a href="#">Fou</a></li>
+                <li><a href="#">Roi</a></li>
+                <li><a href="#">Dame</a></li>
+                <li><a href="#">Pion</a></li>
+                <li><a href="#">Cavalier</a></li>
+                <li><a href="#">Tour</a></li>
+            </ul>
+            <h1>Règle du jeu</h1>
+        </div>
+    </body>
+</html>
