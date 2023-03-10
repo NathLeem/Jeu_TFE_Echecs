46933cc94fd7b49dfa80cbc55fef21bed58bb713
Leemans Nathan <200253@CCM-Asty-Moulin.local>
2023-03-10 11:57:59 +0100

    avancement site regle jeu

---
+++ Regle_jeu/element/footer.php
@@ -0,0 +1,24 @@
+<table class="footer">
+    <tr>
+        <div class="footerblock">
+            <td><p>Follow us</p></td>
+        </div>
+        <div class="footerblock">
+            <td>
+                <a href="https://twitter.com/">
+                    <img src="picture/icon1.jpg" alt="twitter">
+                </a>
+            </td>
+            <td>
+                <a href="https://www.facebook.com/">
+                    <img src="picture/icon2.jpg" alt="facebook">
+                </a>
+            </td>
+            <td>
+                <a href="https://www.google.be/?hl=fr">
+                    <img src="picture/icon3.jpg" alt="google">
+                </a>
+            </td>
+        </div>
+    </tr>
+</table>
---
+++ Regle_jeu/index.php
@@ -0,0 +1,32 @@
+<?php
+    session_start();
+    require_once 'element/header.php';
+
+    $uri = $_SERVER['REQUEST_URI'];
+    if($uri === '/Regle_jeu/index.php'){
+        require_once 'view/reception.php';
+    }
+    elseif($uri === 'Regle_jeu/index.php?/views/pawn.php'){
+        require_once 'view/pawn.php';
+    }
+    elseif($uri === 'Regle_jeu/index.php?/views/bishop.php'){
+        require_once 'view/bishop.php';
+    }
+    elseif($uri === 'Regle_jeu/index.php?/views/tower.php'){
+        require_once 'view/tower.php';
+    }
+    elseif($uri === 'Regle_jeu/index.php?/views/queen.php'){
+        require_once 'view/queen.php';
+    }
+    elseif($uri === 'Regle_jeu/index.php?/views/king.php'){
+        require_once 'view/king.php';
+    }
+    elseif($uri === 'Regle_jeu/index.php?/views/horse.php'){
+        require_once 'view/horse.php';
+    }
+
+    require_once 'element/footer.php';
+?>
+
+</body>
+</html>
