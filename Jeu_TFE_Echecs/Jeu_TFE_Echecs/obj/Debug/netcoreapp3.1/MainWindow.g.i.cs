cf4690e5c0c5eddd81036c1712f4b64112d1bf8f
Leemans Nathan <200253@CCM-Asty-Moulin.local>
2023-04-11 16:12:25 +0200

    ajout pion

--- Jeu_TFE_Echecs/Jeu_TFE_Echecs/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ Jeu_TFE_Echecs/Jeu_TFE_Echecs/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -101,7 +101,7 @@ namespace Jeu_TFE_Echecs {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Jeu_TFE_Echecs;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Jeu_TFE_Echecs;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
